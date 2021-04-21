f943710f3bde0d1e09bc499ebc047e698417cc44
princewhyte2 <princewhyte02@gmail.com>
2021-04-21 23:29:38 +0100

    CounterApp

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.counterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun increment(view: View) {
+        count ++
+        value.text = count.toString()
+    }
+
+
+
+}
